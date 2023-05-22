a8206f0e516312af4ae9ebd86e4cc792039c91f8
James Sinkala <xinnks@gmail.com>
2023-05-22 14:35:39 +0300

    Refactor: Move db data transactions into api endpoints

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -2,35 +2,14 @@ import rss from '@astrojs/rss';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 import { client } from '../lib/tursoDb';
 import type { APIContext } from 'astro';
+import type { Blog } from '../lib/types';
 
-let posts: any[] = [];
-try {
-  const allPostsResponse = await client.execute({
-    sql: "select posts.title, posts.description, posts.slug, posts.hero, authors.first_name, authors.last_name, authors.slug as author_slug, authors.avatar, posts.content, posts.created_at from posts left join authors on authors.id = posts.author_id order by posts.created_at desc;",
-    args: [],
-  });
-  posts = allPostsResponse.rows.map((post) => {
-    return {
-      published: false,
-      title: post.title,
-      description: post.description,
-      slug: post.slug,
-      hero: post.hero,
-      created_at: post.created_at,
-      publish_date: post.publish_date,
-      author: {
-        first_name: post.first_name,
-        last_name: post.last_name,
-        slug: post.slug,
-        avatar: post.avatar
-      }
-    }
-  });
-} catch (error) {
-  // TODO: Handle error and notify user
-}
 
 export async function get(context: APIContext) {
+  let posts: Blog[] = [];
+  const response = await fetch(context.site + 'all-posts.json');
+  posts = await response.json() as unknown as Blog[];
+
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
