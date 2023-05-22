6f81db1cbda53f66a89ab09bb1eac2e686f5cd00
James Sinkala <xinnks@gmail.com>
2023-05-22 13:35:41 +0300

    fix: Use edgeless functions

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import netlify from "@astrojs/netlify/edge-functions";
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
